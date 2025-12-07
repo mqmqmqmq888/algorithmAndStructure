e84b943dbe9cf1262efccf02544d858d9b92375a
mq <1922976005@qq.com>
2025-12-07 18:21:44 +0800

    创建main.cpp文件写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+	
+}
+
+int main()
+{
+	int arr[] = {12,3,4,52,34,22,44};
+	int size = sizeof(arr)/sizeof(int);
+	sort(arr,size);
+	return 0;
+}
